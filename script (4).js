fb865e3486dc79e05505524a193bf2111db36255
pavel <dubovikdmc@gmail.com>
2023-03-11 16:13:10 +0300

    Initial commit

---
+++ script (4).js
@@ -0,0 +1,8 @@
+function bebra(name){
+  
+  if(name.toUpperCase().indexOf('viagra')){}
+return name.indexOf('viagra').toUpperCase() || name.indexOf('XXX').toUpperCase();
+}
+
+console.log(bebra("i lil viagra"))
+
